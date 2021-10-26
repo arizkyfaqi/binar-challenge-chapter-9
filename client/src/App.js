390b9a64d769904ae7fa42e7e149e3971440b3bb
adhi305 <nugrahadhi24@gmail.com>
2021-10-26 21:33:44 +0700

    landingpage update

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import CarouselBox from "./components/CarouselBox";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+        <CarouselBox />
+        <Footer />
+      </Router>
+    </>
   );
 }
 
---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import react from "react";
+import "../../App.css";
+import HeroSection from "../HeroSection";
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
+
+export default Home;
