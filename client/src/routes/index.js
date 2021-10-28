0929f023a02a8e195b15c41a6962b27029756975
adhi305 <nugrahadhi24@gmail.com>
2021-10-28 21:01:24 +0700

    update

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,15 +1,5 @@
-import { lazy } from "react";
 import Home from "../components/pages/Home";
-// import EditProfile from "../components/EditPage";
-// import App from "../components/onChange";
 import App from "../components/ProfileOnChange";
-// import ChangePassword from "../components/PasswordPage";
-// import ProfilePage from "../components/ProfilePage";
-
-// const CreateUser = lazy(() => import("../components/CreateUser"));
-// const SearchUser = lazy(() => import("../components/SearchUser"));
-// const EditUser = lazy(() => import("../components/EditUser"));
-// const ProfilePage = lazy(() => import("../components/EditUser"));
 
 const routes = [
   {
@@ -28,6 +18,7 @@ const routes = [
     component: "Not Found",
     exact: false,
   },
+
 ];
 
 export default routes;
