6c6e44af4736a36bb254707e5fd42ab0df698c9d
Irfannudien <muhammadirfannudien@gmail.com>
2021-11-03 14:24:17 +0700

    update db, edit profile, onsubmit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const db = require("./database");
 const pool = require("./database");
 
 // Middleware
